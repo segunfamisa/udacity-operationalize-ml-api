996f6c189d351ea603dc38a4d003367397820479
Segun Famisa <segun.famisa@soundcloud.com>
2020-08-12 00:50:33 +0200

    Add commands to build and run the docker container

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
-
-# Step 1:
 # Build image and add a descriptive tag
+docker image build --tag=python-flask-ml .
 
-# Step 2: 
 # List docker images
+docker image ls
 
-# Step 3: 
 # Run flask app
+docker run -p 8000:80 python-flask-ml
